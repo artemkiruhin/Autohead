db3c44d9f5e2cffd79b4934cf2d3e1467ddf8755
artemkiruhin <temkaunik@gmail.com>
2024-11-23 11:49:57 +0300

    IStore created

---
+++ AutoHead.Core/Stores/IStore.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoHead.Core.Stores;
+
+public interface IStore <TEntity>
+{
+    Task Add(TEntity model);
+    Task Delete(Guid id);
+    Task Update(TEntity model);
+    Task<TEntity?> GetById(Guid id);
+    Task<IEnumerable<TEntity>> GetAll();
+}
